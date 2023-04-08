81cb4930ab23191398c4c6b0c971935d0e5b1673
Koreen Rohde <koreen.rohde@gmail.com>
2023-04-07 22:01:51 -0500

    Video at 33:34 - MVP for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Header from "./components/Header/Header"
-
-
+import css from './styles/app.module.scss'
 const App = () => {
-  return <Header/>
+  return <div className={`bg-primary ${css.container}`}>
+      <Header/>
+  </div>
 };
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import css from './Header.module.scss'
+import { BiPhoneCall } from 'react-icons/bi'
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className={`paddings ${css.wrapper}`}>
+
+            <div className={`flexCenter innerWidth ${css.container}`}>
+                <div className={css.name}>
+                    Koreen
+                </div>
+
+                <ul className={`flexCenter ${css.menu}`}>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Experience</a></li>
+                    <li><a href="">Portfolio</a></li>
+                    <li><a href="">Testimonials</a></li>
+                    <li>
+                        <p>123 456 7890</p>
+                        <BiPhoneCall size={"40px"} />
+                    </li>
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
