3def572e725cdbb30ec5439bfd241673763af08a
Koreen Rohde <koreen.rohde@gmail.com>
2023-05-25 21:35:47 -0500

    Stopped at 1:18 50 seconds

--- src/components/Expertise/Expertise.jsx
+++ src/components/Expertise/Expertise.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { projectExperience } from '../../utils/data'
-import css from './Expertise.module.scss'
+import React from "react";
+import { projectExperience } from "../../utils/data";
+import css from "./Expertise.module.scss";
 const Expertise = () => {
-    return (
-        <section className={css.wrapper}>
-            <div 
-                className={` paddings yPaddings flexCenter innerWidth ${css.container}`}
-                >
-
-                <div className={css.leftSide}>
-                    {projectExperience.map((exp, i) => {
-                            return (
-                            <div className={css.exp} key={i}>
-                                <div className="flexCenter">
-                                    <exp.icon size={25} color="white" />
-                                </div>
-
-                                <div>
-                                    <span>{exp.name}</span>
-                                    <span>{exp.projects}</span>
-                                </div>
-                            </div>
-                        );
-                    })}
-                     </div>
-
-                <div className={css.rightSide}>
+  return (
+    <section className={css.wrapper}>
+      <div
+        className={`paddings yPaddings flexCenter innerWidth ${css.container}`}
+      >
+        <div className={css.leftSide}>
+          {projectExperience.map((exp, i) => {
+            return (
+              <div className={css.exp} key={i}>
+                <div className="flexCenter">
+                  <exp.icon size={25} color="white" />
                 </div>
+
+                <div>
+                  <span>{exp.name}</span>
+                  <span className="secondaryText">{exp.projects} Projects</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className={css.rightSide}>
+            
+            right side
+            
             </div>
-        </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
 export default Expertise
